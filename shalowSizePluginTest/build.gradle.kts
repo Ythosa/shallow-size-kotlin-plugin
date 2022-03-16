362ee5aeb5729dd93e4d646b41ed730e91319c3a
Ruslan Babin <vasus714@yandex.ru>
2022-03-16 04:18:42 +0300

    feat: detekt plugin

--- shalowSizePluginTest/build.gradle.kts
+++ shalowSizePluginTest/build.gradle.kts
@@ -3,12 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val junitVersion: String by project
 val jvmTargetVersion: String by project
 
+plugins {
+    id("io.gitlab.arturbosch.detekt") version "1.19.0"
+}
+
 dependencies {
     implementation("junit:junit:4.13.2")
     implementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.19.0")
 }
 
 tasks.compileKotlin {
@@ -32,4 +36,9 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     dependsOn(":shallowSizePlugin:assemble")
+}
+
+detekt {
+    parallel = true
+    autoCorrect = true
 }
